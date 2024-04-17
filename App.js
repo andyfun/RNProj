6e2058fda2cb8a3e8321b59385a83ca48807f3ea
andy <779683637@qq.com>
2024-04-18 00:14:20 +0800

    m

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import { Button,FlatList,Image, TouchableHighlight } from 'react-native-web';
+import { StyleSheet, Text, View, Dimensions, Button,FlatList,Image, TouchableHighlight, Platform} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import { useEffect, useState } from 'react';
@@ -43,6 +42,8 @@ function GirlsLike() {
         />
     </TouchableHighlight>
   );
+  console.log("use eff------2222------->"+currentMargin.marginStart);
+  console.log("use eff------3333------->"+Platform.OS);
   return (
     <View style={styles.container}>
      <FlatList 
@@ -66,31 +67,59 @@ function CarsDriver() {
     </View>
   );
 }
-
+function ZhengtuDriver() {
+  return (
+    <View style={styles.container}>
+      <View>
+          <Button title="ZhengtuDriver" />
+      </View>
+       
+    </View>
+  );
+}
 function App() {
   return (
     <NavigationContainer>
         <Drawer.Navigator  initialRouteName="Grils">
-          <Drawer.Screen name="Grils" component={GirlsLike} />
-          <Drawer.Screen name="CarsDriver" component={CarsDriver} />
+          <Drawer.Screen name="女生专属" component={GirlsLike} />
+          <Drawer.Screen name="赛车" component={CarsDriver} />
+          <Drawer.Screen name="国战" component={ZhengtuDriver} />
         </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+const WEB_MARGIN = 50;
+const marginConfig= {
+    web:{
+      marginStart: WEB_MARGIN,
+      marginEnd: WEB_MARGIN,
+    },
+ default:{
+      marginStart: 0,
+      marginEnd: 0,
+}
+};
+
+const currentMargin = marginConfig[Platform.OS] || marginConfig.default;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     flexWrap: 'wrap',
+    marginStart: currentMargin.marginStart,
+    marginEnd: currentMargin.marginEnd,
+    
   },
   
   image: {
     resizeMode: "cover",
     justifyContent: "center",
-    width: 200,
-    height: 200,
-    borderRadius: 20,
-   
+    width: 177,
+    height: 133,
+    borderRadius: 30,
+    marginTop: 10,
+    marginStart: 10,
+    marginEnd: 10,
   },
   button:{
     width: 200,
@@ -98,16 +127,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   item: {
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 20,
+ 
+   
   },
   listViewStyle:{
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+     
   }
   
 });
